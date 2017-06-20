677a1655b919bfdf5a2ed7d3ce324c7841519efd
yousalty <tntdabber@gmail.com>
2017-06-19 15:00:26 -1000

    Good to go

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function fortuneTeller(){
-  
-}
+    var numOfChildren = 0
+    var partnersName = "BESTPERSONEVER";
+    var geographical = "Rich Part of Oahu"
+    var jobTitle = ""
+
+ console.log("You will be a X in Y, and be married to Z with N kids")
+
+ console.log("You will be a " + jobTitle + " in " +
+ 	geographicalLocation + " , and be married to " + partnersName + "with" + numberOfChildren + " kids ")
+
 
 function ageCalculator(){
 
